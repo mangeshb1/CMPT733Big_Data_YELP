87c33af624bcc21ce08f7cebfa8f364e3129082d
jna50 <jna50@sfu.ca>
2016-03-26 14:37:07 -0800

    adding UI files

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '', 'mydm') or die("Could not connect.");
+
+
+?>
---
+++ search.php
@@ -0,0 +1,12 @@
+<?php
+    include ('connection.php');
+    $key=$_GET['key'];
+    $array = array();
+
+    $query = $db->query("select * from user where name LIKE '%{$key}%'");
+    while($row = $query->fetch_assoc())
+    {
+      $array[] = $row['name'];
+    }
+    echo json_encode($array);
+?>
