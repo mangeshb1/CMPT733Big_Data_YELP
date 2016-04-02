d19e6ff51d0224adde4f577e38dcfd9c59ff31db
Saif Charaniya <saif@d207-023-207-129.wireless.sfu.ca>
2016-04-02 11:47:34 -0700

    merge

--- sentiment_analysis/sentiment_analysis_ToneAnalyzerV3Beta.py
+++ sentiment_analysis/sentiment_analysis_ToneAnalyzerV3Beta.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SQLContext
-from watson_developer_cloud import ToneAnalyzerV1Experimental
+#from watson_developer_cloud import ToneAnalyzerV1Experimental
 from watson_developer_cloud import ToneAnalyzerV3Beta
 import json,time
+import sys
 from pyspark.sql.types import *
 from pyspark.sql.functions import udf
 from pyspark import SparkContext,SparkConf
@@ -10,19 +11,21 @@ conf = SparkConf().setAppName("IBM Watson Sentimemnt Analysis")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-#dfIP = sqlContext.read.json("/home/ub51/SFU/733Project/Data/yelp_academic_dataset_review.json").cache()
-dfIP_loc = "../lv_data/lv_ibm_short/lv_ibm_input.csv"
-dfIP = sqlContext.read.format('com.databricks.spark.csv')\
-     .options(header='true', inferschema='true').load(dfIP_loc)
+l = "../lv_data/ibm_watson_input_one"
+dfIP = sqlContext.parquetFile(sys.argv[1]).cache()
+#dfIP_loc = "../lv_data/lv_ibm_short/lv_ibm_input.csv"
+#dfIP = sqlContext.read.format('com.databricks.spark.csv')\
+#     .options(header='true', inferschema='true').load(dfIP_loc)
 
 dfIP.printSchema()
 
 #no need to filter anymore
 #dfIP.registerTempTable("dfIP")
-#print dfIP.count()
+print ("COUNT HERE!!! : ", dfIP.count())
 #dfIPFilter = sqlContext.sql("select * from dfIP where votes.useful > 70")
 #print dfIPFilter.count()
 
+#dfIP = dfIP.dropna(how='any')
 
 def getSentiment(s):
 
@@ -35,7 +38,11 @@ def getSentiment(s):
     password='aj0qwsv4D7uu',
     version='2016-02-11')
     time.sleep(1)
-    myvar = tone_analyzer.tone(text=s)
+    try:
+        myvar = tone_analyzer.tone(text=s)
+    except:
+        return [];
+    
     temp1 = myvar['document_tone']
     temp2 = temp1['tone_categories']
 
@@ -52,8 +59,11 @@ def getSentiment(s):
 udfProtToEncoding=udf(lambda s:getSentiment(s), ArrayType(StringType()))
 
 ans = dfIP.withColumn("Sentiment", udfProtToEncoding(dfIP.review_text))
-#ans.show(3,False)
+ans.show(3,False)
+
+#print ans.count()
+#print ("END COUNT HERE: ", ans.count())
 
 #save 
-ans.write.parquet("sentiment_output.parquet")
+ans.write.parquet(sys.argv[2])
 
