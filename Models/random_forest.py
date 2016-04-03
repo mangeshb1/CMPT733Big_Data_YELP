7c7ef03aeaadeaae8e0ae5584b4846ad6c394092
mangeshb1 <mangeshb1@yahoo.com>
2016-04-02 23:03:46 -0700

    Adding Spark Evaluation Matrix measures for Random Forest

--- Models/random_forest.py
+++ Models/random_forest.py
@@ -1,47 +1,52 @@
-# Run a random forrest classification model on the data las vegas dataset
-# print relavant stats
-
 from pyspark.mllib.tree import RandomForest, RandomForestModel
-from pyspark.mllib.util import MLUtils
-from pyspark.mllib.linalg import SparseVector
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.sql import SQLContext
-from pyspark.sql.types import *
-from pyspark import SparkContext,SparkConf
+from pyspark.mllib.evaluation import MulticlassMetrics
 
-conf = SparkConf().setAppName("KMeans")
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-
-#dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
-#dfModelDataAll.show()
+dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
+dfModelDataAll.show()
 
 #dfnoID = dfModelDataAll.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
 
 #dfnoID.show()
 
-data_loc = "lv_cluster_long.parquet"
-data_df = sqlContext.parquetFile(data_loc)
+(trainingData1, testData) = dfModelDataAll.randomSplit([0.7, 0.3])
 
-(trainingData, testData) = data_df.randomSplit([0.7, 0.3])
+trainingData = trainingData1.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
 
-trainLab = trainingData.map(lambda l:LabeledPoint(l.cluster, l.vector))
+trainingData.show()
+testData.show()
 
-testLab = testData.map(lambda l:LabeledPoint(l.cluster, l.vector))
+trainLab = trainingData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
+
+testLab = testData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
 
 
 model = RandomForest.trainClassifier(trainLab, numClasses=3,\
-                     categoricalFeaturesInfo={},  numTrees=3,\
-                              impurity='entropy')
+                                     categoricalFeaturesInfo={},  numTrees=3,\
+                                    impurity='entropy')
 print "Model Ready"
 
 predictions = model.predict(testLab.map(lambda x: x.features))
 
 labelsAndPredictions = testLab.map(lambda lp: lp.label).zip(predictions)
-
-finalRdd = testData.map(lambda rec:(rec.yelp_id,rec.stars)).zip(predictions) 
+finalRdd = testData.map(lambda rec:(rec.yelp_id,rec.yelp_score)).zip(predictions) 
 
 testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testLab.count())
 print('Test Error = ' + str(testErr))
 print('Learned classification forest model:')
 print(model.toDebugString())
+
+#Code for Spark Evaluation Matrix
+
+metrics = MulticlassMetrics(labelsAndPredictions)
+# Overall statistics
+precision = metrics.precision()
+recall = metrics.recall()
+f1Score = metrics.fMeasure()
+confusionMatrx = metrics.confusionMatrix()
+
+print("All Evaluation Measures Stats")
+print("Confusion Matix = %s" % confusionMatrx)
+print("Precision = %s" % precision)
+print("Recall = %s" % recall)
+print("F1 Score = %s" % f1Score)
