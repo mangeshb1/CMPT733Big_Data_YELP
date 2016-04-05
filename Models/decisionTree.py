800e23f290f2342bbd720f6a14d3c3f62e745dd6
Saif Charaniya <saif@d207-023-207-129.wireless.sfu.ca>
2016-04-04 17:34:38 -0700

    updated code for models to get full data precision

--- Models/decisionTree.py
+++ Models/decisionTree.py
@@ -3,34 +3,42 @@ from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from pyspark.mllib.util import MLUtils
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.mllib.linalg import SparseVector
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark import SparkContext,SparkConf
+from pyspark.mllib.evaluation import MulticlassMetrics
 
-dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
-dfModelDataAll.show()
+conf = SparkConf().setAppName("KMeans")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+#dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
+#dfModelDataAll.show()
 
 #dfnoID = dfModelDataAll.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
 
 #dfnoID.show()
 
-(trainingData1, testData) = dfModelDataAll.randomSplit([0.7, 0.3])
+data_loc = "../lv_data/lv_class_input.parquet"
+data_df = sqlContext.parquetFile(data_loc)
 
-trainingData = trainingData1.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
+(trainingData, testData) = data_df.randomSplit([0.7, 0.3])
 
-trainingData.show()
-testData.show()
+trainLab = trainingData.map(lambda l:LabeledPoint(l.risk, l.vector))
 
-trainLab = trainingData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
-
-testLab = testData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
+testLab = testData.map(lambda l:LabeledPoint(l.risk, l.vector))
 
 
-model = DecisionTree.trainClassifier(trainLab, numClasses=2, categoricalFeaturesInfo={},
-                                     impurity='gini', maxDepth=5, maxBins=32)
+model = DecisionTree.trainClassifier(trainLab, numClasses=3, categoricalFeaturesInfo={},
+                    impurity='entropy', maxDepth=10, maxBins=32)
 
 print "Model Ready"
 
 predictions = model.predict(testLab.map(lambda x: x.features))
 
 labelsAndPredictions = testLab.map(lambda lp: lp.label).zip(predictions)
+labelsAndPredictions = labelsAndPredictions.map(lambda (a,b): (b,a))
 
 finalRdd = testData.map(lambda rec:(rec.yelp_id,rec.yelp_score)).zip(predictions) 
 testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testLab.count())
@@ -47,8 +55,57 @@ recall = metrics.recall()
 f1Score = metrics.fMeasure()
 confusionMatrx = metrics.confusionMatrix()
 
+print ("DECISION TREE FOR TEST SET ONLY")
 print("All Evaluation Measures Stats")
 print("Confusion Matix = %s" % confusionMatrx)
 print("Precision = %s" % precision)
 print("Recall = %s" % recall)
 print("F1 Score = %s" % f1Score)
+
+
+#now perform decision tree on all data
+testLab = data_df.map(lambda l:LabeledPoint(l.risk, l.vector))
+
+predictions = model.predict(testLab.map(lambda x: x.features))
+
+labelsAndPredictions = testLab.map(lambda lp: lp.label).zip(predictions)
+labelsAndPredictions = labelsAndPredictions.map(lambda (a,b): (b,a))
+
+
+finalRdd = data_df.map(lambda rec:(rec.yelp_id, \
+                    rec.name, rec.vector[0], rec.risk)).zip(predictions) 
+
+testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testLab.count())
+print('Test Error = ' + str(testErr))
+print('Learned classification forest model:')
+print(model.toDebugString())
+
+metrics = MulticlassMetrics(labelsAndPredictions)
+# Overall statistics
+precision = metrics.precision()
+recall = metrics.recall()
+f1Score = metrics.fMeasure()
+confusionMatrx = metrics.confusionMatrix()
+
+print("DECISION TREE FOR ALL DATA: ")
+print("All Evaluation Measures Stats")
+print("Confusion Matix = %s" % confusionMatrx)
+print("Precision = %s" % precision)
+print("Recall = %s" % recall)
+print("F1 Score = %s" % f1Score)
+
+#save output
+#finalRdd.coalesce(1).saveAsTextFile("decision_tree_output73")
+
+
+#run on the boston data:
+boston = sqlContext.parquetFile("../boston_data/boston_vectors")
+predictions = model.predict(boston.map(lambda x: x.vector))
+
+
+
+finalRdd = boston.map(lambda rec:(rec.yelp_id)).zip(predictions) 
+
+final_df = sqlContext.createDataFrame(finalRdd, ["yelp_id", "risk"])
+final_df.write.parquet("boston_classified", mode='overwrite')
+final_df.show()
--- Models/random_forest.py
+++ Models/random_forest.py
@@ -1,42 +1,56 @@
+# Run a random forrest classification model on the data las vegas dataset
+# print relavant stats
+
 from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.linalg import SparseVector
 from pyspark.mllib.regression import LabeledPoint
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark import SparkContext,SparkConf
 from pyspark.mllib.evaluation import MulticlassMetrics
 
-dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
-dfModelDataAll.show()
+conf = SparkConf().setAppName("KMeans")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+#dfModelDataAll = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/home/ub51/SFU/733Project/Data/model.csv')
+#dfModelDataAll.show()
 
 #dfnoID = dfModelDataAll.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
 
 #dfnoID.show()
 
-(trainingData1, testData) = dfModelDataAll.randomSplit([0.7, 0.3])
+data_loc = "../lv_data/lv_class_input.parquet"
+#data_loc = "lv_cluster_long.parquet"
+data_df = sqlContext.parquetFile(data_loc)
 
-trainingData = trainingData1.select('yelp_score','sum_anger','sum_happy','sum_conf','sum_disgust','no_vio','risk_rating')
+#data_df = data_df.withColumn("risk", data_df.cluster)
 
-trainingData.show()
-testData.show()
+(trainingData, testData) = data_df.randomSplit([0.7, 0.3])
 
-trainLab = trainingData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
+trainLab = trainingData.map(lambda l:LabeledPoint(l.risk, l.vector))
 
-testLab = testData.map(lambda l:LabeledPoint(l.risk_rating,[l.yelp_score,l.sum_anger,l.sum_happy,l.sum_conf,l.sum_disgust,l.no_vio]))
+testLab = testData.map(lambda l:LabeledPoint(l.risk, l.vector))
 
 
 model = RandomForest.trainClassifier(trainLab, numClasses=3,\
-                                     categoricalFeaturesInfo={},  numTrees=3,\
-                                    impurity='entropy')
+                     categoricalFeaturesInfo={},  numTrees=3,\
+                              impurity='entropy')
 print "Model Ready"
 
 predictions = model.predict(testLab.map(lambda x: x.features))
 
 labelsAndPredictions = testLab.map(lambda lp: lp.label).zip(predictions)
-finalRdd = testData.map(lambda rec:(rec.yelp_id,rec.yelp_score)).zip(predictions) 
+labelsAndPredictions = labelsAndPredictions.map(lambda (a,b): (b,a))
+
+
+finalRdd = testData.map(lambda rec:(rec.yelp_id,rec.stars)).zip(predictions) 
 
 testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testLab.count())
 print('Test Error = ' + str(testErr))
 print('Learned classification forest model:')
 print(model.toDebugString())
-
-#Code for Spark Evaluation Matrix
 
 metrics = MulticlassMetrics(labelsAndPredictions)
 # Overall statistics
@@ -45,8 +59,44 @@ recall = metrics.recall()
 f1Score = metrics.fMeasure()
 confusionMatrx = metrics.confusionMatrix()
 
+print("RANDOM FORREST FOR TEST SET ONLY: ")
 print("All Evaluation Measures Stats")
 print("Confusion Matix = %s" % confusionMatrx)
 print("Precision = %s" % precision)
 print("Recall = %s" % recall)
 print("F1 Score = %s" % f1Score)
+
+
+#now perform random forrest on all data
+testLab = data_df.map(lambda l:LabeledPoint(l.risk, l.vector))
+
+predictions = model.predict(testLab.map(lambda x: x.features))
+
+labelsAndPredictions = testLab.map(lambda lp: lp.label).zip(predictions)
+#labelsAndPredictions = labelsAndPredictions.map(lambda (a,b): (b,a))
+
+
+finalRdd = data_df.map(lambda rec:(rec.yelp_id, rec.name, \
+                                       rec.vector[0], rec.risk)).zip(predictions)
+
+testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testLab.count())
+print('Test Error = ' + str(testErr))
+print('Learned classification forest model:')
+print(model.toDebugString())
+
+metrics = MulticlassMetrics(labelsAndPredictions)
+# Overall statistics
+precision = metrics.precision()
+recall = metrics.recall()
+f1Score = metrics.fMeasure()
+confusionMatrx = metrics.confusionMatrix()
+
+print("RANDOM FORREST FOR ALL DATA: ")
+print("All Evaluation Measures Stats")
+print("Confusion Matix = %s" % confusionMatrx)
+print("Precision = %s" % precision)
+print("Recall = %s" % recall)
+print("F1 Score = %s" % f1Score)
+
+#save outcome
+finalRdd.coalesce(1).saveAsTextFile("random_forrest_output73")
